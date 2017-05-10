a57cd0c189747663605a65dd499fce46ac4a18ae
ingo <ingo.bingo@somewhere.com>
2017-05-10 15:14:32 +0200

    Projects page addes, header modified

--- server.js
+++ server.js
@@ -54,6 +54,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'project page'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'error01'
